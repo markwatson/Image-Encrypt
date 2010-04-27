72d3264819185ffda23e277251c5d4a23b57ad16
Mark Watson <mw5300@gmail.com>
2010-04-27 03:28:42 -0600

    added key expansion. TODO: test it

--- aesencrypt.h
+++ aesencrypt.h
@@ -1,7 +1,7 @@
 #ifndef AESENCRYPT_H
 #define AESENCRYPT_H
 
-#define BLOCK_SIZE 64
+#define BLOCK_SIZE 4
 
 #include <string>
 
@@ -11,12 +11,31 @@ private:
 	int Nk, Nb, Nr; // lengths.
 	std::string textkey;
 	char* fullkey;
+	char* expandedkey;
 public:
 	aesEncrypt();
 	~aesEncrypt();
 	bool encryptBlock(char*);
 	void setTextKey(std::string);
 	void expandKey();
+
+	unsigned char sboxify(int val);
+	unsigned char roundify(int index);
+
+	void sboxify(unsigned char * tmp) {
+		tmp[0] = sboxify(tmp[0]);
+		tmp[1] = sboxify(tmp[1]);
+		tmp[2] = sboxify(tmp[2]);
+		tmp[3] = sboxify(tmp[3]);
+	}
+
+	void rotate(unsigned char * tmp) {
+		unsigned char trimmings;
+		trimmings = tmp[0];
+		tmp[0] = tmp[1];
+		tmp[1] = tmp[2];
+		tmp[3] = trimmings;
+	}
 };
 
 #endif // AESENCRYPT_H
